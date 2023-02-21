d7ba0634c50f3a02dcc5a351fd06a61cd8262363
Sagar Suresh Srikant <108785364+sagarsrikantcodes@users.noreply.github.com>
2023-02-21 08:36:02 -0700

    Add files via upload

--- 101_programming_foundations/lesson_2_small_programs/calculator.js
+++ 101_programming_foundations/lesson_2_small_programs/calculator.js
@@ -1,29 +1,80 @@
-// Welcoming the user to our program 
-console.log('Welcome to Calculator!');
-// Ask the user to enter the first number
-let rlSync = require('readline-sync');
-let number1 = Number(rlSync.question("What's the first number? "));
-console.log(number1);
-
-// Ask the user to enter the second number
-let number2 = Number(rlSync.question("what's the second number? "));
-console.log(number2);
-
+// Ask the user for the first number.
+// Ask the user for the second number.
 // Ask the user for an operation to perform.
-let operation = rlSync.question("Enter the operation to perform 1) Add 2) Subtract 3) Multiply 4) Divide");
-console.log(operation);
-
 // Perform the operation on the two numbers.
-let result = 0;
-if (operation === '1') {
-  result = number1 + number2;
-} else if (operation === '2') {
-  result = number1 - number2;
-} else if (operation === '3') {
-  result = number1 * number2;
-} else if (operation === '4') {
-  result = number1 / number2;
+// Print the result to the terminal.
+
+// Function to check for invalid number
+function invalidNumber(number) {
+  return  number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-// Print the result to the terminal.
-console.log(`The result is: ${result}`);
+const readline = require('readline-sync');
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+prompt('Welcome to Calculator!');
+
+prompt("What's the first number?");
+let number1 = readline.question();
+
+while (invalidNumber(number1)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  prompt("What's the first number?");
+  number1 = readline.question();
+}
+
+//console.log(number1);
+
+prompt("What's the second number?");
+let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  prompt("What's the second number?");
+  number2 = readline.question();
+}
+
+// console.log(`${number1} ${number2}`);
+prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3 or 4');
+  operation = readline.question();
+}
+
+let result;
+
+/* If else statement
+if (operation === "1") {
+  result = Number(number1) + Number(number2);
+  // console.log(`The addition of ${number1} and ${number2} is ${result}`);
+} else if (operation === "2") {
+  result = Number(number1) - Number(number2);
+} else if (operation === "3" ) {
+  result = Number(number1) * Number(number2);
+} else if (operation === "4") {
+  result = Number(number1) / Number(number2);
+}
+*/
+
+switch (operation) {
+  case '1' :
+    result = Number(number1) + Number(number2);
+    break;
+  case '2' :
+    result = Number(number1) - Number(number2);
+    break;
+  case '3' :
+    result = Number(number1) * Number(number2);
+    break;
+  case '4' :
+    result = Number(number1) / Number(number2);
+    break;
+}
+
+console.log(`The result is ${result}`);
+
