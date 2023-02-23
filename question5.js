236029a7c5b9d7a0b8462acd26d608038d5c1163
Sagar Suresh Srikant <108785364+sagarsrikantcodes@users.noreply.github.com>
2023-02-23 12:05:10 -0700

    Delete question5.js

--- question5.js
+++
@@ -1,3 +0,0 @@
-// Problem Statement - What will be the output of the following code?
-console.log(false == '0'); // logs true since there is implicit type conversion
-console.log(false === '0'); // logs false since there is strict equality comparison 
